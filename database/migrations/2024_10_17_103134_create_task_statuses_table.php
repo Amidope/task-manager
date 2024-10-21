25ae10e1b48cfd69ead9bb182da35104f621cf72
Amidope <noreply>
2024-10-21 20:08:57 +0500

    add seed

--- database/migrations/2024_10_17_103134_create_task_statuses_table.php
+++ database/migrations/2024_10_17_103134_create_task_statuses_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->timestamp('created_at')->useCurrent();
         });
+        Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
     }
 
     /**
