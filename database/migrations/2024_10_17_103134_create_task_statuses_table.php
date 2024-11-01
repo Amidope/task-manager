a849b458260fbd625305318642163e81dc5b71e2
Amidope <noreply>
2024-11-01 19:19:52 +0500

    rm seeder

--- database/migrations/2024_10_17_103134_create_task_statuses_table.php
+++ database/migrations/2024_10_17_103134_create_task_statuses_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->timestamp('created_at')->useCurrent();
         });
-        Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
+//        Artisan::call('db:seed', ['--class' => 'DatabaseSeeder']);
     }
 
     /**
