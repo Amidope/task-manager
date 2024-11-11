be13145710f1bbf8f814ebcf49f9a2b6c00aae6b
Amidope <noreply>
2024-11-11 14:27:20 +0500

    refactor

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,15 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::resource('/task_statuses', TaskStatusController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('/task_statuses', TaskStatusController::class)->except('show');
-});
-
-Route::middleware('guest')->group(function () {
-    Route::resource('/task_statuses', TaskStatusController::class)->only('index');
 });
 
 require __DIR__.'/auth.php';
