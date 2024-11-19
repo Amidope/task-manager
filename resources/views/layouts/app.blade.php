0f86dda70fa4a97a85c23ae6a8835d27eefd5ee1
Amidope <noreply>
2024-11-19 18:34:44 +0500

    add csrf token for ujs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-param" content="_token" />
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -11,7 +12,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
-{{--        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">--}}
+
         @notifyCss
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
