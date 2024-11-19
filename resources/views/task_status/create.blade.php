4b35c5393eabf6d9774f39ac8d8369ca2ed450d9
Amidope <noreply>
2024-11-19 18:39:18 +0500

    refactor

--- resources/views/task_status/create.blade.php
+++ resources/views/task_status/create.blade.php
@@ -11,7 +11,7 @@
                 {{--            first:mt-0 mt-6--}}
                 {{  html()->input('text', 'name')->required()->class('mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500/3') }}
                 @error('name')
-                <div class="text-rose-600">{{ $message }}</div>
+                    <div class="text-rose-600">{{ $message }}</div>
                 @enderror
                 {{  html()->submit(__('views.task_status.create.buttons.create'))
                         ->class('mt-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent
