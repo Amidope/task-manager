5dbcccec94df1666a0c86e1adffa6dfb200a012c
Amidope <noreply>
2025-01-16 18:21:26 +0500

    add resources

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\LabelController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use App\Models\Label;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,7 +18,8 @@ Route::get('/dashboard', function () {
 
 Route::resources([
     'task_statuses' => TaskStatusController::class,
-    'labels' => LabelController::class
+    'labels' => LabelController::class,
+    'tasks' => TaskController::class
 ]);
 
 Route::middleware('auth')->group(function () {
