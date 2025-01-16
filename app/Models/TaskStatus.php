bc1b576237af20ba75a877a37ee3bb8292e11a1d
Amidope <noreply>
2025-01-16 18:19:32 +0500

    add relations

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TaskStatus extends Model
 {
@@ -22,4 +23,9 @@ class TaskStatus extends Model
      * @var bool
      */
     public $timestamps = false;
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'status_id');
+    }
 }
