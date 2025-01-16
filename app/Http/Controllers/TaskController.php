0a7bb06815276fa42c1b9127edad1dfa267e752b
Amidope <noreply>
2025-01-16 18:08:09 +0500

    fill task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,16 +4,50 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Label;
 use App\Models\Task;
+use App\Models\TaskStatus;
+use App\Models\User;
+use Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Redirect;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class TaskController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Task::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $tasks = QueryBuilder::for(Task::class)
+            ->allowedFilters([
+                'name',
+                AllowedFilter::exact('status_id'),
+                AllowedFilter::exact('created_by_id'),
+                AllowedFilter::exact('assigned_to_id'),
+            ])
+            ->orderBy('id')
+            ->paginate(15);
+
+        $filterParams = $request->input('filter', []);
+        $taskStatusesForFilterForm = TaskStatus::pluck('name', 'id');
+        $usersForFilterForm = User::pluck('name', 'id');
+
+
+        return view('task.index', compact(
+            'filterParams',
+            'tasks',
+            'taskStatusesForFilterForm',
+            'usersForFilterForm',
+        ));
     }
 
     /**
@@ -21,7 +55,11 @@ class TaskController extends Controller
      */
     public function create()
     {
-        //
+        $task = new Task();
+        $taskStatuses = TaskStatus::all();
+        $users = User::all();
+        $labels = Label::all();
+        return view('task.create', compact('task', 'taskStatuses', 'users', 'labels'));
     }
 
     /**
@@ -29,7 +67,15 @@ class TaskController extends Controller
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $currentUser = Auth::user();
+        $task = $currentUser->createdTasks()->make($validated);
+        $task->save();
+
+        $labels =  Arr::get($validated, 'labels', []);
+        $task->labels()->attach($labels);
+        notify()->success(__('flashes.tasks.store.success'));
+        return Redirect::route('tasks.index');
     }
 
     /**
@@ -37,7 +83,7 @@ class TaskController extends Controller
      */
     public function show(Task $task)
     {
-        //
+        return view('task.show', compact('task'));
     }
 
     /**
@@ -45,7 +91,10 @@ class TaskController extends Controller
      */
     public function edit(Task $task)
     {
-        //
+        $taskStatuses = TaskStatus::all();
+        $users = User::all();
+        $labels = Label::all();
+        return view('task.edit', compact('task', 'taskStatuses', 'users', 'labels'));
     }
 
     /**
@@ -53,7 +102,14 @@ class TaskController extends Controller
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $validated = $request->validated();
+        $task->fill($validated)->save();
+
+        $labels = Arr::get($validated, 'labels', []);
+        $task->labels()->sync($labels);
+        notify()->success(__('flashes.tasks.updated'));
+
+        return Redirect::route('tasks.index');
     }
 
     /**
@@ -61,6 +117,9 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->labels()->detach();
+        $task->delete();
+        notify()->success(__('flashes.tasks.deleted'));
+        return Redirect::route('tasks.index');
     }
 }
