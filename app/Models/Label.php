5c6841eacc02b70d801561e9e41439aeb9f23cbf
Amidope <noreply>
2025-01-16 18:15:01 +0500

    add task relation

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Label extends Model
 {
@@ -17,4 +18,8 @@ class Label extends Model
      */
     protected $fillable = ['name', 'description'];
 
+    public function tasks(): BelongsToMany
+    {
+        return $this->belongsToMany(Task::class);
+    }
 }
