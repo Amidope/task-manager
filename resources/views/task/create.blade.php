c3266a8284a4f1afd5b4ebce2b867d9983773ad6
Amidope <noreply>
2025-01-28 19:13:48 +0500

    fix header

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        {{ __('views.task.show.header') }}
+        {{ __('views.task.create.form_header') }}
     </x-slot>
 
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8  space-y-6">
